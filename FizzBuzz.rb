6efb84d1e740618b590b9430883ce595810ade0c
originalcybernaut <59384941+originalcybernaut@users.noreply.github.com>
2022-10-12 01:58:48 -0400

    Create FizzBuzz.rb

---
+++ FizzBuzz.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+puts "Please input a maximum integer for FizzBuzz test"
+n = gets.chomp.to_i rescue false
+puts "========================="
+def fizz_buzz(n)
+    for i in 0..n
+      if i%3 == 0 and i%5 == 0
+        puts "FizzBuzz"
+      elsif i%3 == 0
+        puts "Fizz"
+      elsif i%5 == 0
+        puts "Buzz"
+      else
+        puts i
+      end
+    end
+end
+
+fizz_buzz(n)

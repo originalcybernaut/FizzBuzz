93d6690340c5f2f9493fd500f5be339437bb8ed9
originalcybernaut <59384941+originalcybernaut@users.noreply.github.com>
2022-10-19 00:31:41 -0400

    Add files via upload

---
+++ FizzBuzz.ps1
@@ -0,0 +1,21 @@
+# Get-ExecutionPolicy
+# Set-ExecutionPolicy -Scope Process unrestricted
+## don't forget to change it back! Close PS session when done!
+
+
+$in = Read-Host "Enter maximum FizzBuzz value"
+
+for($i=1; $i -le $in; $i++){
+	if (($i % 5 -eq 0) -and ($i % 3 -eq 0)){ 
+		Write-host "FizzBuzz"
+	}
+	elseif ($i %3 -eq 0){
+		Write-host "Fizz"
+	}
+	elseif ($i %5 -eq 0){
+		Write-host "Buzz"
+	}
+	else{
+		Write-host $i
+	}
+}
